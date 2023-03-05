311813376297ceb9d7f5bb0e4e91e4a35d7c4ca9
Jakir <jakirsayyed98@gmail.com>
2023-03-05 10:54:38 +0530

    games

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const auth = require('./middlwear/middelwear');
 const PORT = process.env.PORT || 2000;
 const url =process.env.MONGO_URL || "mongodb+srv://jakirsayyed98:5ypFCNhcZbYT2QMB@cluster0.quz1kf2.mongodb.net/?retryWrites=true&w=majority";
 const UserRoutes = require('./Routes/UserRoute')
+var fetchUrl = require("fetch").fetchUrl;
 
 
 
 app.get('/games',(req,res)=>{
-    fetch('https://pub.gamezop.com/v3/games?id=4625').then((result)=>{
-        res.json({errorcode:"0",message:"Sucessfull",games:result.json()})
-    })
+
+    fetchUrl("https://pub.gamezop.com/v3/games?id=4625", function(error, meta, body){
+        
+        var data = body.toString()
+        res.json({errorcode:"0",message:"Sucessfull",games:JSON.parse(data)})
+    });
+
+    // fetch('https://pub.gamezop.com/v3/games?id=4625').then((result)=>{
+    //     res.json({errorcode:"0",message:"Sucessfull",games:result.json()})
+    // })
 })
 
 
